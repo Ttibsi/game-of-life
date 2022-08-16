4fdc85dfb7dd54d0abdc70d0f745ee4ba7d3deff
Ttibsi <ashisbitt@icloud.com>
2022-08-16 14:46:03 +0100

    render shaders

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,73 @@
 
 // https://stackoverflow.com/a/67585643
 
+// This is a openGL Shading Language file stored in a c-style string
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+void renderShaders() {
+    // Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Fragment Shader - used for specific shapes on the screen
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Error checking
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // Link shaders to a shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    // Linking Vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glUseProgram(shaderProgram);
+
+    // After it's linked to the program, the shaders aren't needed and can be
+    // deleted
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
 void renderContent(GLFWwindow *window) {
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
@@ -20,6 +87,8 @@ void renderContent(GLFWwindow *window) {
         glBindBuffer(GL_ARRAY_BUFFER, buffer_obj);
         glBufferData(GL_ARRAY_BUFFER, (6 * sizeof(float)), &tri_1,
                      GL_STATIC_DRAW);
+
+        renderShaders();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
