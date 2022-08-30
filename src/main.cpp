fb19d6e551f62c263f868c11e5df8c14d6a62d12
Ttibsi <ashisbitt@icloud.com>
2022-08-30 17:32:50 +0100

    Coloured background

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,11 @@ int main() {
     // Render Loop
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
+
+        // Render here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
