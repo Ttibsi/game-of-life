444bd4c87dfe3d8c80e9253acc2d787ad8711cbc
Ttibsi <ashisbitt@icloud.com>
2022-10-03 12:00:00 +0100

    Populate board function

--- src/game.cpp
+++ src/game.cpp
@@ -2,6 +2,12 @@
 
 #include <iostream>
 
+board_t populate_board(board_t b) {
+    int places_to_fill[5] = {2, 4, 6, 8};
+    for (int i : places_to_fill) { b[i].toggle = true; }
+    return b;
+}
+
 void print_board(board_t b, int size) {
     for (auto &pair : b) {
         if (pair.toggle == true) {
@@ -28,6 +34,7 @@ board_t construct_board(int size) {
 }
 
 void main_game(int size, int iter) {
-    board_t my_Board = construct_board(size);
+    // board_t my_Board = construct_board(size);
+    board_t my_Board = populate_board(construct_board(size));
     print_board(my_Board, size);
 }
--- src/game.hpp
+++ src/game.hpp
@@ -3,6 +3,7 @@
 
 #include "board.hpp"
 
+board_t populate_board(board_t b); 
 void print_board(board_t b, int size); 
 board_t construct_board(int size); 
 void main_game(int size, int iter); 
