a620b6beddc4423d4886e6113fcff5f77e2e22f6
Ttibsi <ashisbitt@icloud.com>
2022-10-31 16:59:05 +0000

    json parsing from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <utility>
 
 #include "../include/argparse/argparse.hpp"
+#include "../include/nlohmann/json.hpp"
 #include "game.hpp"
+
+using json = nlohmann::json;
 
 std::string version = "v0.1.0";
 
@@ -18,10 +22,23 @@ int main(int argc, char *argv[]) {
         .default_value(1)
         .scan<'i', int>()
         .help("Iteration of the board state to output");
+    program.add_argument("-j", "--json")
+        .default_value("")
+        // .scan<'i', std::string>()
+        .help("Pass in a JSON config file");
 
     program.parse_args(argc, argv);
-    int size = program.get<int>("--size");
-    int iter = program.get<int>("--iteration");
+    std::ifstream jsonstream(program.get<std::string>("--json").c_str());
+    json config = json::parse(jsonstream);
+    std::cout << "json size: " << config.size() << "\n";
 
-    main_game(size, iter);
+    // if (config.size() == 0 ) {
+    //     int size = program.get<int>("--size");
+    //     int iter = program.get<int>("--iteration");
+    // } else {
+    //     int size = config[size];
+    //     int iter = config[iter];
+    // }
+    //
+    // main_game(size, iter, config);
 }
